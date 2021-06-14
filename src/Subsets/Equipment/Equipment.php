9fd8b104a24c22d4ff0b73992c77d1c2636da21f
totov <totov@users.noreply.github.com>
2021-06-14 16:57:10 +0000

    Fix styling

--- src/Subsets/Equipment/Equipment.php
+++ src/Subsets/Equipment/Equipment.php
@@ -2,12 +2,12 @@
 
 namespace Totov\Cap\Subsets\Equipment;
 
+use Totov\Cap\Exceptions\AuthorisationFailedException;
 use Totov\Cap\Subsets\Equipment\Requests\ByDerivativeTypeAndCapIdRequest;
 use Totov\Cap\Subsets\Equipment\Requests\ByVin;
 use Totov\Cap\Subsets\Equipment\Requests\ByVinAndVrm;
 use Totov\Cap\Subsets\Equipment\Requests\ByVrm;
 use Totov\Cap\Subsets\Equipment\Requests\SupportedDerivativeTypesRequest;
-use Totov\Cap\Exceptions\AuthorisationFailedException;
 use Totov\Cap\Subsets\Subset;
 
 class Equipment extends Subset
